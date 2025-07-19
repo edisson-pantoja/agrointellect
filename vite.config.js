d813d661f5022408d81bcd79d6efd1a93307b044
edisson-pantoja <74325941+edisson-pantoja@users.noreply.github.com>
2025-07-19 13:26:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // aponta para o diretório atual como raiz
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   }
 });
